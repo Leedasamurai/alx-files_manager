b00a9eab483de4f3a7e299cb67c61a2b27e7431b
Leedasamurai <lesegoleemac@gmail.com>
2024-08-05 10:14:55 +0000

    client created

---
+++ utils/redis.js
@@ -0,0 +1,42 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.error(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+
+    // Promisify the get, set, and del functions
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
+export default redisClient;
