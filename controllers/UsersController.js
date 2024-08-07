ab181ed6ef5856d43ca37aafa608ee0c534564af
Leedasamurai <lesegoleemac@gmail.com>
2024-08-07 00:58:54 -0700

    auth the new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 const crypto = require('crypto');
+const { ObjectId } = require('mongodb');
 const { db } = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 class UsersController {
   static async postNew(req, res) {
@@ -26,6 +28,32 @@ class UsersController {
       const newUser = result.ops[0];
       return res.status(201).json({ id: newUser._id.toString(), email: newUser.email });
     } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    try {
+      const userId = await redisClient.get(`auth_${token}`);
+
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const user = await db.collection('users').findOne({ _id: ObjectId(userId) });
+
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      return res.status(200).json({ id: user._id.toString(), email: user.email });
+    } catch (err) {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
